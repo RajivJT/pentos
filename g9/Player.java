2e98959d2fcdfd5c41be5d95983048bdc3a1e841
Rajiv Thamburaj <rjt2132@columbia.edu>
2016-09-15 10:06:38 -0400

    Add empty Player class.

---
+++ g9/Player.java
@@ -0,0 +1,17 @@
+package pentos.g0;
+
+import pentos.sim.Cell;
+import pentos.sim.Building;
+import pentos.sim.Land;
+import pentos.sim.Move;
+
+import java.util.*;
+
+public class Player implements pentos.sim.Player {
+
+    public void init() {
+    }
+    
+    public Move play(Building request, Land land) {
+    }
+}
