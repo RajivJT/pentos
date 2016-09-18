69a725f3343401a7e3c6f5d7ae02d705700348f5
Rajiv Thamburaj <rjt2132@columbia.edu>
2016-09-18 18:09:04 -0400

    Modify BFS to only build roads along the perimeter of existing developments.

--- g9/Player.java
+++ g9/Player.java
@@ -9,9 +9,10 @@ import java.util.*;
 
 public class Player implements pentos.sim.Player {
 
-    private Set<Cell> road_cells = new HashSet<Cell>();
+    private Set<Cell> road_cells;
 
     public void init() {
+        road_cells = new HashSet<Cell>();
     }
     
     public Move play(Building request, Land land) {
@@ -25,7 +26,8 @@ public class Player implements pentos.sim.Player {
                     for (int ri = 0 ; ri < rotations.length ; ri++) {
                         Building b = rotations[ri];
                         if (land.buildable(b, p)) {
-                            moves.add(new Move(true, request, p, ri, new HashSet<Cell>(), new HashSet<Cell>(), new HashSet<Cell>()));
+                            moves.add(new Move(true, request, p, ri, new HashSet<Cell>(),
+                                      new HashSet<Cell>(), new HashSet<Cell>()));
                         }
                     }
                 }
@@ -38,7 +40,8 @@ public class Player implements pentos.sim.Player {
                     for (int ri = 0 ; ri < rotations.length ; ri++) {
                         Building b = rotations[ri];
                         if (land.buildable(b, p)) {
-                            moves.add(new Move(true, request, p, ri, new HashSet<Cell>(), new HashSet<Cell>(), new HashSet<Cell>()));
+                            moves.add(new Move(true, request, p, ri, new HashSet<Cell>(),
+                                      new HashSet<Cell>(), new HashSet<Cell>()));
                         }
                     }
                 }
@@ -46,7 +49,6 @@ public class Player implements pentos.sim.Player {
         }
 
         if (moves.isEmpty()) {
-            System.out.print("COULD NOT PLACE BUILDING " + request.toString() + "\n");
             return new Move(false);
         }
 
@@ -61,13 +63,25 @@ public class Player implements pentos.sim.Player {
                 road_cells.addAll(roadCells);
                 chosen.road = roadCells;
                 return chosen;
-            } else {
-                System.out.print("COULD NOT BUILD ROAD" + request.toString() + "\n");
-                continue;
             }
         }
 
         return new Move(false);
+    }
+
+    private boolean isPerimeter(Land land, Cell cell) {
+        int i = cell.i;
+        int j = cell.j;
+
+        for (int di = -1; di <= 1; di++) {
+            for (int dj = -1; dj <= 1; dj++) {
+                if (di != 0 && dj != 0 && !land.unoccupied(i+di, j+dj)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 
@@ -75,47 +89,68 @@ public class Player implements pentos.sim.Player {
         Set<Cell> output = new HashSet<Cell>();
         boolean[][] checked = new boolean[land.side][land.side];
         Queue<Cell> queue = new LinkedList<Cell>();
-        // add border cells that don't have a road currently
-        Cell source = new Cell(Integer.MAX_VALUE,Integer.MAX_VALUE); // dummy cell to serve as road connector to perimeter cells
+
+        Cell source = new Cell(Integer.MAX_VALUE,Integer.MAX_VALUE);
+
         for (int z=0; z<land.side; z++) {
-            if (b.contains(new Cell(0,z)) || b.contains(new Cell(z,0)) || b.contains(new Cell(land.side-1,z)) || b.contains(new Cell(z,land.side-1))) //if already on border don't build any roads
-            return output;
-            if (land.unoccupied(0,z))
-            queue.add(new Cell(0,z,source));
-            if (land.unoccupied(z,0))
-            queue.add(new Cell(z,0,source));
-            if (land.unoccupied(z,land.side-1))
-            queue.add(new Cell(z,land.side-1,source));
-            if (land.unoccupied(land.side-1,z))
-            queue.add(new Cell(land.side-1,z,source));
+            if (b.contains(new Cell(0,z)) ||
+                b.contains(new Cell(z,0)) ||
+                b.contains(new Cell(land.side-1,z)) ||
+                b.contains(new Cell(z,land.side-1))) {
+                return output;
+            }
+
+            if (land.unoccupied(0,z)) {
+                queue.add(new Cell(0,z,source));
+            }
+
+            if (land.unoccupied(z,0)) {
+                queue.add(new Cell(z,0,source));
+            }
+
+            if (land.unoccupied(z,land.side-1)) {
+                queue.add(new Cell(z,land.side-1,source));
+            }
+
+            if (land.unoccupied(land.side-1,z)) {
+                queue.add(new Cell(land.side-1,z,source));
+            }
         }
+
         // add cells adjacent to current road cells
         for (Cell p : road_cells) {
             for (Cell q : p.neighbors()) {
-            if (!road_cells.contains(q) && land.unoccupied(q) && !b.contains(q)) 
-                queue.add(new Cell(q.i,q.j,p)); // use tail field of cell to keep track of previous road cell during the search
+                if (!road_cells.contains(q) && land.unoccupied(q) && !b.contains(q) &&
+                    isPerimeter(land, q)) {
+                    queue.add(new Cell(q.i,q.j,p));
+                }
             }
-        }	
+        }
+
         while (!queue.isEmpty()) {
             Cell p = queue.remove();
             checked[p.i][p.j] = true;
-            for (Cell x : p.neighbors()) {		
-            if (b.contains(x)) { // trace back through search tree to find path
-                Cell tail = p;
-                while (!b.contains(tail) && !road_cells.contains(tail) && !tail.equals(source)) {
-                output.add(new Cell(tail.i,tail.j));
-                tail = tail.previous;
-                }
-                if (!output.isEmpty())
-                return output;
-            }
-            else if (!checked[x.i][x.j] && land.unoccupied(x.i,x.j)) {
-                x.previous = p;
-                queue.add(x);	      
-            } 
 
+            for (Cell x : p.neighbors()) {
+                if (b.contains(x)) { // trace back through search tree to find path
+                    Cell tail = p;
+                    while (!b.contains(tail) && !road_cells.contains(tail) &&
+                           !tail.equals(source)) {
+                        output.add(new Cell(tail.i,tail.j));
+                        tail = tail.previous;
+                    }
+
+                    if (!output.isEmpty()) {
+                        return output;
+                    }
+                } else if (!checked[x.i][x.j] && land.unoccupied(x.i,x.j) &&
+                           isPerimeter(land, x)) {
+                    x.previous = p;
+                    queue.add(x);
+                } 
             }
         }
+
         if (output.isEmpty() && queue.isEmpty())
             return null;
         else
